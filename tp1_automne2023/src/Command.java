92198e00f26ec9b3188afa5e86c8aaf025d6acbe
alessmcs <alessandramancas23@gmail.com>
2023-10-06 11:37:38 -0400

    Added classes

---
+++ tp1_automne2023/src/Command.java
@@ -0,0 +1,9 @@
+public class Command {
+
+    public static void readFile( String fileName){
+        // 2 premieres lignes indiquent m x n -> faire des tableaux
+        Grille grille = new Grille();
+
+    }
+
+}
