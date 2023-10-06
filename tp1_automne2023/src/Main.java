db1b13d6522be365db8af87d505fdcbdef55563a
alessmcs <alessandramancas23@gmail.com>
2023-10-06 11:28:19 -0400

    Added TODOs

--- tp1_automne2023/src/Main.java
+++ tp1_automne2023/src/Main.java
@@ -1,5 +1,10 @@
+
+// TODO:  lire le fichier txt pour identifier la grille
+// TODO: si un mot est le préfixe d'un autre, pas besoin de refaire la recherche
+// TODO: arbre pour la liste de mots pour eviter de refaire la recherche
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
 }
