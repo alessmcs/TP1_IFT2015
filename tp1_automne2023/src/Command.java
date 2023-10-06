18b5b52a36e81ce4c84149e093b781d183a39622
oceane-hays <128166831+oceane-hays@users.noreply.github.com>
2023-10-06 12:12:13 -0400

    readFile

--- tp1_automne2023/src/Command.java
+++ tp1_automne2023/src/Command.java
@@ -1,9 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Command {
 
     public static void readFile( String fileName){
         // 2 premieres lignes indiquent m x n -> faire des tableaux
-        Grille grille = new Grille();
+
+        Arbre tree = new Arbre();
+
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader br = new BufferedReader(fileReader);
+            String line = br.readLine();
+            String[] dimension = line.split("\\s+");
+
+            int colonne = Integer.parseInt(dimension[0]);
+            int ligne = Integer.parseInt(dimension[1]);
+
+            Grille grille = new Grille(colonne,ligne);
+
+            } catch (IOException var) {
+            System.err.println("Error reading file: " + var.getMessage());
+            PrintStream e = System.out;
+            File x = new File(fileName);
+            e.println("Absolute path:" + x.getAbsolutePath());
+        }
 
     }
+
+
+
 
 }
--- tp1_automne2023/src/Grille.java
+++ tp1_automne2023/src/Grille.java
@@ -1,3 +1,19 @@
 public class Grille {
 
+    private int colonne;
+    private int ligne;
+
+    public Grille(int colonne , int ligne) {
+        this.colonne = colonne;
+        this.ligne = ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+
+    public int getLigne() {
+        return ligne;
+    }
+
 }
